fccfe42ec7ad5f5a53ffdb9c9c8067d803edbda4
Rebecca Anne Sealfon <ras2198@columbia.edu>
2013-02-05 11:57:56 -0500

    Initialized empty Rails file

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Careerfriend::Application.config.session_store :cookie_store, key: '_careerfriend_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Careerfriend::Application.config.session_store :active_record_store
