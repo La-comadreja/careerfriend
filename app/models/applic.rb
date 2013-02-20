e1a6d4da44e7ea774bb52c91569f92b71028d4bd
Rebecca Anne Sealfon <ras2198@columbia.edu>
2013-02-20 17:33:07 -0500

    Applic and Referral belong to job, user and annotated

--- app/models/applic.rb
+++ app/models/applic.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: applics
+#
+#  id         :integer          not null, primary key
+#  message    :text
+#  resume_id  :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Applic < ActiveRecord::Base
+  belongs_to :job
+  belongs_to :user
   attr_accessible :message, :resume_id
 end
--- test/unit/applic_test.rb
+++ test/unit/applic_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: applics
+#
+#  id         :integer          not null, primary key
+#  message    :text
+#  resume_id  :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class ApplicTest < ActiveSupport::TestCase
