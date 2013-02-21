4f964c85da9e880508e8d9da7edb57c831a6188a
Rebecca Anne Sealfon <ras2198@columbia.edu>
2013-02-21 18:11:04 -0500

    New Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Careerfriend::Application.config.secret_token = '47e74d1ff1c0ef62fbd87eff98c672bd77b9427f8385ecc0131d051c863cf642173e40514a4c3091ccba9afc32467a45106984f795306f03a6bf6293252ec40b'
