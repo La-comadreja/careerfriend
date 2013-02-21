4aaaf08b22e617a020ede2d9b16c18bac898881d
Rebecca Anne Sealfon <ras2198@columbia.edu>
2013-02-21 17:52:37 -0500

    Remove all files

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Careerfriend::Application.config.secret_token = 'cdea721e1ffd6dc4b4deb8bb5b6bb42195e23b3c8ef45de8e370bc20778d399cbd046ade0440d5fbbcf8e9279719a6edc3565131396da6232b0ad5ea47e16911'
