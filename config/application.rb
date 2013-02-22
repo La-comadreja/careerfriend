9da673953ae56efa63b2b0c0b0eaf48df1527a39
Rebecca Anne Sealfon <ras2198@columbia.edu>
2013-02-21 21:04:44 -0500

    Try precompile again

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ require 'rails/all'
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
-  config.assets.precompile += ['job_template.css']
 
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
@@ -13,6 +12,8 @@ end
 
 module Careerfriend
   class Application < Rails::Application
+    config.assets.precompile += ['job_template.css']
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
