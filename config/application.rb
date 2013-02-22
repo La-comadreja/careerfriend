33f6d407c2a108a0971353030acf1c5e59dbd4d1
Rebecca Anne Sealfon <ras2198@columbia.edu>
2013-02-21 21:00:19 -0500

    Precompile job_template.css

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,8 @@ require 'rails/all'
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
+  config.assets.precompile += ['job_template.css']
+
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
